2ee00bf588734570c0a87014b1aadc66f49b484c
SpeechBanana <speechbanana@jhu.edu>
2021-10-28 00:08:37 +0100

    Update AlignPhonemes.m

--- AlignPhonemes.m
+++ AlignPhonemes.m
@@ -53,8 +53,8 @@ end
 for i = 1 : lenX
     for j = 1 : lenY
         % we evaluate three things
-        [costdel, delInd] = del_cost(P{i,j+1}); %cost if you deleted X{i}
-        [costins, insInd] = ins_cost(P{i+1,j}); %cost if you inserted Y{j}
+        [costdel, delInd]   = del_cost(P{i,j+1}); %cost if you deleted  X{i}
+        [costins, insInd]   = ins_cost(P{i+1,j}); %cost if you inserted Y{j}
         [costsub, subInd] = sub_cost(X{i},Y{j},P{i,j}, vowdict, consdict, mannerdict);
         allInds = [delInd,insInd,subInd];
         Ddel = D(i+1-1,j+1) + costdel*(1-(i==lenX)*(1-low));
@@ -101,8 +101,10 @@ else
 end
 end
 
-% below are simple costs
+% below are simple costs for the current operation which is insertion
 function [cost, ind] = ins_cost(Parr)
+%%%Table 2 (left) in Ratnanather et al. (2022)
+%%%previous op: Parr{i}(1)==2 is insertion
 for i = 1 : length(Parr)
 	if Parr{i}(1) == 2
 		cost = 1;
@@ -110,11 +112,15 @@ for i = 1 : length(Parr)
 		return
 	end
 end
+%%%else previous op is sub/del 
 cost = 1.5;
 ind = 1;
 end
 
+% below are simple costs for the current operation which is deletion
 function [cost, ind] = del_cost(Parr)
+%%%Table 2 (left) in Ratnanather et al. (2022)
+%%%previous op: Parr{i}(1)==1 is deletion
 for i = 1 : length(Parr)
 	if Parr{i}(1) == 1
 		cost = 1;
@@ -122,41 +128,56 @@ for i = 1 : length(Parr)
 		return
 	end
 end
+%%%else previous op is ins/sub
 cost = 1.5;
 ind = 1;
 end
 
 % a better one, consonants and vowels and smilarity deductions
+%Table 2 (right) in Ratnanather et al. (2022)
 function [cost, ind] = sub_cost(symbol1,symbol2,Parr, vowdict, consdict, mannerdict)
 one_is_vowel = sum(symbol1(1)=='AEIOU');
 two_is_vowel = sum(symbol2(1)=='AEIOU');
 best = 10;
 ind = 1;
 for i = 1 : length(Parr)
-	%initialize currcost according to whether the last op was a deletion
-	if Parr{i}(1) == 1
+	%initialize currcost according to whether the last op was 
+    %a deletion which has an additive cost of 0.5
+    %a insertion which has an additive cost of 0.1
+    %a match which has zero cost
+	if Parr{i}(1) == 1 %%% deletion
 		currcost = 0.5;
-    elseif Parr{i}(1) == 2
+    elseif Parr{i}(1) == 2 %%% insertion
         currcost = 0.1;
-    else
+    else %%%match
 		currcost = 0;
 	end
  
+%%% Current operation is substitution
+%%% If previous operation was insertion or deletion
 	if xor(one_is_vowel, two_is_vowel)
+%%% vowel-consonant or vice-versa
 		currcost = currcost + 5;
 	elseif ~one_is_vowel
+%%% consonant-consonant
 		currcost = currcost + 1.75;
 		if isKey(consdict, symbol1) && sum(strcmp(symbol2, consdict(symbol1)))
-%                 fidc = fopen('ConsPairs.txt','a');
-%                 fprintf(fidc,'%s %s\n',symbol1,symbol2);
-%                 fclose(fidc);
+%           fidc = fopen('ConsPairs.txt','a');
+%           fprintf(fidc,'%s %s\n',symbol1,symbol2);
+%           fclose(fidc);
+%%%         decrement for similar consonant i.e. 1.2 = 1.75 - 0.55
 			currcost = currcost - .55;
 		elseif isKey(mannerdict, symbol1) && sum(strcmp(symbol2, mannerdict(symbol1)))
-%                 fidm = fopen('MannerPairs.txt','a');
-%                 fprintf(fidm,'%s %s\n',symbol1,symbol2);
-%                 fclose(fidm);
+%           fidm = fopen('MannerPairs.txt','a');
+%           fprintf(fidm,'%s %s\n',symbol1,symbol2);
+%           fclose(fidm);
+%%%         decrement for same manner consonant i.e. 1.3 = 1.75 - 0.45
 			currcost = currcost - .45;
 		elseif strcmp(symbol1,symbol2)
+%%%         matched consonants: suppose the initialised currcost is 0.5 (Parr{i}(1)==1, deletion) then must
+%%%         decrement by 1.75 and a further 0.3 to bring down to 
+%%%         0.2 if previous op was deletion or 0.1 if previous op was insertion
+%%%         otherwise  if it was 0.1 (Parr{i}(1) == 2, insertion) then the result is a negative number because it is a matched 
 			currcost = currcost - 1.75 - 0.3;
 			if currcost < 0
 				cost = 0;
@@ -165,13 +186,19 @@ for i = 1 : length(Parr)
 			end
 		end
 	elseif one_is_vowel
+%%%     vowel-vowel
 		currcost = currcost + 0.9;
 		if isKey(vowdict, symbol1) && sum(strcmp(symbol2, vowdict(symbol1)))
-%                 fidv= fopen('VowPairs.txt','a');
-%                 fprintf(fidv,'%s %s\n',symbol1,symbol2);
-%                 fclose(fidv);
+%           fidv= fopen('VowPairs.txt','a');
+%           fprintf(fidv,'%s %s\n',symbol1,symbol2);
+%           fclose(fidv);
+%%%         decrement for similar vowel i.e. 0.65 = 0.9 - 0.25
 			currcost = currcost - .25;
 		elseif strcmp(symbol1,symbol2)
+%%%         matched vowels: suppose the initialised currcost is 0.5 (Parr{i}(1)==1, deletion) then must
+%%%         decrement by 0.9 and a further 0.3 to bring down to 
+%%%         0.2 if previous op was deletion or 0.1 if previous op was insertion
+%%%         otherwise  if it was 0.1 (Parr{i}(1) == 2, insertion) then the result is a negative number because it is a matched 
 			currcost = currcost - 0.9 - 0.3;
 			if currcost < 0
 				cost = 0;
